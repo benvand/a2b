7edfe07c9a20f66915719f86a949bc119ac8a4bb
benvand <benjaminfvandersteen@gmail.com>
2018-08-04 09:19:09 +0100

    enc ga file?

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,12 +1,26 @@
-/**
- * Created by benvandersteen on 12/10/16.
- */
-
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
-ga('create', 'XXXXXXXX', 'auto');
-ga('send', 'pageview');
-
+U2FsdGVkX18REAAAAAAAABi6IuQrxr61pduZQpdthY+2rMuwxuixGkgwtwKGQtHY
+rQBtK95bOLU0p3WV98b5stHYizl7jJJ2Apcq9I1DTCsMtav6N8ls0XWiePM1z0eQ
+hi362S4d9RvyiT4GbIxG4MnZHhfCx9zNZKz1XIyZRi2vBurKuqwIgExP59rYeBpM
+G1i+Obe87NzpTaZG1kijiOrioRQxb3Z9r4/Gm5RTcSf5/6cAN8GJn0yfZ1azUuMN
+TF6QDc09Jv9uBdqkaiAHgR6MZ8Qbfc6Zvm8HagHVhMvVvEyskYG+8Azh2w1pjLYm
+Vz/K/B2BXEK5rYq0T/rP0OVP9rOTLTTX5Gno+CmUKKO5rvXe5oFh3IJjk/e8YDHD
+ohi0pm5V4kQuoawD5iTQ+7aezaF0+q1niUYCjKf/C2if4hKffOydVzrZJa3yiZka
++KxGv8TrsC7XM1udmdaJ9wc3BewF7Rj5sq3orXdBB+lgEQo4l6VhYKGKVp0Rv9q0
+WgyMwkzcw34YGl51wmfBqxd4QJQoEkOtHgdMBZ61PnNMP4hepIopm/5xOuGkLRxE
+E8A9igX1nEGSIF95TK+Ap1/JV3AStl3/mThMtyYWGlbBB7/YECt3tRXaC+daZsF0
+DR7boVLccxqH7FE3/cz97h1p1q5feEdet/gIwltiXuS9ZaJh5oTD+twg8VhFKB6C
+V5thzI45FZ9PhUCL2Euy+4SoWpVFl1LMnskPJlRxL/aOCfhXfRd2OyCzpcbazFwt
+uTdlko/zn2GlW95SqUovSCTUKVMrv2528BrceEf75qPrycSXZD0SBcvD7lU+Ofqg
+dnn6WQLWNC/dh0YLmJMULqVBxSr+yJZuNTCjqKxcP7fNK0pIgBGkFoldfBvii/fe
+VzArM9jGXltRAwxGu51zeZQblz6ccMGF5gDghbAP3qeodPmrhpLvIy6MV9YDytsX
+f7fwNHDKShzs/q901njnl4HKBFnCESsDb+8YEqXnWIKOsNVIxENPS49jLfdoWdvJ
+aLHMKukODAr42GwTXdEwNcNhQw3uZF8mL/sFa434ERcPfUOS/L67FK3Ol0OdglGf
++WtcXhLxiJwvxnW85Af/sdqDxWyetSA1Xr7twjtPAdpo39rAXgrc7Q6zDPSGAjaJ
+cqLqWt566r1Cgimp248GcyMqKl6j3GLYMZCJK63SjweJAhe+P9V2jyBdwLPxUlJ7
+QAmuUeHEGVEenXjXvuLs5+qNWzG5RpR3ZDyBedDODBuu0KJQuMjAuBELHeYWTy1E
+TAbutSzWJrQ90RN2MNsjG063GYXE4XIixj58Ze3tHWKVRNugW1CNc8IULM4yjF7V
+vCkQZqVdxA9mnjK11RBj/yUgm4TsGdqCfLw8kyMzBISXzYdH9TwdWVQkhH/wJoIM
+eDCB5pSb8NcYIVpUZbQuXj3d7o02AYU8dBCZ5dVfS9YZ4h/hAkwiV42DUzXnO0Fh
+g1oVMkVdDmQvnerQ8uMm+7ArKP79uRDKwwTdVO8wn/NMcFpyPai1QXawwEt4D9fT
+UyWpFkCxu4ssQuWwXQ+Bk5oruqy90lY42W0tzaSBpgw/omSmcdNBtNNIOfaLdWTG
+ilemg+0D8xbSPHasMyD3g8nED3rxXbhVGd2ulz09GOI=
